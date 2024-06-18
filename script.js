eee04fde538691f17662b49c29ac574454d4a6ef
Vinz <abc@xyz.com>
2024-06-19 00:26:35 +0100

    menufixed

--- script.js
+++ script.js
@@ -2,7 +2,13 @@ document.addEventListener("DOMContentLoaded", () => {
  const hamburger = document.querySelector(".hamburger");
  const navlinks = document.querySelector(".navlinks");
 
+
  hamburger.addEventListener("click", () => {
   navlinks.classList.toggle("active");
+  hamburger.classList.toggle("active");
  })
 })
+
+  
+
+
